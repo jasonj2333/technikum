ef544b15f5418b26f6465565fe7e29a5ddc95af2
Jerzy Jasonek <jasonj2333@gmail.com>
2022-09-06 18:36:07 +0200

    Update 18_1_cw

--- INF04/Programowanie_obiektowe/18_Szablony/18_1_cw.cpp
+++ INF04/Programowanie_obiektowe/18_Szablony/18_1_cw.cpp
@@ -1,32 +1,30 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include<iostream>
-//#include <math.h>//M_PI           3.14159265358979323846  /* pi */
 using namespace std;
 
 //Definicja szablonu - wzorca funkcji polePr()
 template <typename T> //Deklaracja parametru T szablonu
-T poleKo(T pPromien, double pi) {
-    return (pi * (pPromien * pPromien));
+T poleKo(T pPromien) {
+    return (M_PI * (pPromien * pPromien));
 }
 
 
 //Definicja szablonu - wzorca funkcji obwodPr()
 template <typename T> //Deklaracja parametru T szablonu
-T obwodKo(T pPromien, double pi) {
-    return (2 * pi * pPromien);
+T obwodKo(T pPromien) {
+    return (2 * M_PI * pPromien);
 }
 
 int main()
 {
     float promien{ 1.5 };
-    cout << "Wartość liczby pi wynosi: " << M_PI << endl;
-    cout << "Pole wynosi: " << poleKo<int>(promien, M_PI) << endl;
-    cout << "Obwód wynosi: " << obwodKo<int>(promien, M_PI) << endl;
+    cout << "Pole wynosi: " << poleKo<int>(promien) << endl;
+    cout << "Obwód wynosi: " << obwodKo<int>(promien) << endl;
 
     cout << endl;
-    cout << "Pole wynosi: " << poleKo(promien, M_PI) << endl;
-    cout << "Obwód wynosi: " << obwodKo(promien, M_PI) << endl;
+    cout << "Pole wynosi: " << poleKo(promien) << endl;
+    cout << "Obwód wynosi: " << obwodKo(promien) << endl;
 
     return 0;
 }
