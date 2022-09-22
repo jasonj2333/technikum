23c36bd82598ecb89a9847e9fca8a156cd4a0455
Jerzy Jasonek <jasonj2333@gmail.com>
2022-09-22 21:50:48 +0200

    add northwind_select

---
+++ INF03/BazyDanych/SQL-polecenia/northwind_select.sql
@@ -0,0 +1,30 @@
+-- https://dbase.jasonek.pl/northwind/
+
+-- Zapytania zapisz w pliku kwerendy.txt - plik prześlij na maila jerzy.jasonek@gmail.com
+
+
+-- 1. Pokaż wszystkie kategorie produktów
+
+-- 2. Pokaż wszystkie rekordy z tabeli `Order Details`
+
+-- 3. Wybierz nazwy i adresy wszystkich klientów
+
+-- 4. Wybierz nazwiska i numery telefonów pracowników
+
+-- 5. Wybierz nazwy i ceny produktów
+
+-- 6. Pokaż nazwy i adresy stron www dostawców
+
+-- 7. Wybierz nazwy i adresy wszystkich klientów mających siedziby w Londynie
+
+-- 8. Znajdź numer zamówienia oraz identyfikator klienta dla zamówień z datą wcześniejszą niż '8/1/96'
+
+-- 9. Wybierz nazwy i adresy wszystkich klientów mających siedziby we Francji lub w Hiszpanii
+
+-- 10. Wybierz nazwy i ceny produktów o cenie jednostkowej pomiędzy 20.00 a 30.00
+ 
+-- 11. Wyszukaj informacje o produktach o cenach mniejszych niż 10 lub większych niż 20
+ 
+-- 12. Znajdź produkty, których nie ma w magazynie i które nie zostały jeszcze zamówione
+
+
