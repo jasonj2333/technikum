659412430a46f582783c75f95a2ac0b5387656aa
Jerzy Jasonek <jasonj2333@gmail.com>
2022-11-30 18:40:41 +0100

    update stomatolog

---
+++ INF04/Obiektowe_PHP/13A_PDO_Project/stomatolog/dbh.class.php
@@ -0,0 +1,21 @@
+<?php
+
+class Dbh{
+    private $dbURL = "";
+    private $dbName = "stomatolog.db";
+    
+    public function __construct(){
+        $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+
+        if(strpos($url, 'includes') !== false){
+            $this->dbURL = "../";
+        }       
+    }
+    
+
+    protected function connect(){
+        $pdo = new PDO('sqlite:' . $this->dbURL . $this->dbName);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); //optional
+        return $pdo;
+    }
+}
