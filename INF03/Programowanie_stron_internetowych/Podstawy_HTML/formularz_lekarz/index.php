78415febee9d3c6c96bae1cb47b7a1d633a2caec
Jerzy Jasonek <jasonj2333@gmail.com>
2023-01-23 10:34:47 +0100

    add formularz_lekarz

---
+++ INF03/Programowanie_stron_internetowych/Podstawy_HTML/formularz_lekarz/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rejestracja</title>
+    <style>
+        main{
+            margin: 20px 30px;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <!-- Utwórz formularz rejestracyjny do lekarza z polami:
+        - imie
+        - nazwisko
+        - email
+        - data wizyty
+        - liczba osób na wizycie
+        - zgoda na przetwarzanie danych osobowych
+        - przyciski wyczyść i prześlij -->
+    </main>  
+</body>
+</html>
