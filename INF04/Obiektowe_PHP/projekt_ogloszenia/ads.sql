ba76a330580dbbf9e3f8f79528f5164f6c8b54ed
Jerzy Jasonek <jasonj2333@gmail.com>
2023-02-11 10:47:30 +0100

    Update ogloszenie project

--- INF04/Obiektowe_PHP/projekt_ogloszenia/ads.sql
+++ INF04/Obiektowe_PHP/projekt_ogloszenia/ads.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 22 Sty 2023, 17:10
+-- Czas generowania: 11 Lut 2023, 09:53
 -- Wersja serwera: 10.4.24-MariaDB
 -- Wersja PHP: 8.1.6
 
@@ -11,15 +11,31 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Baza danych: `ads`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ads`
+--
+
+CREATE TABLE `ads` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content` mediumtext DEFAULT NULL,
+  `create_date` datetime DEFAULT current_timestamp(),
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `ads`
+--
+
+INSERT INTO `ads` (`id`, `title`, `content`, `create_date`, `user_id`) VALUES
+(5, 'Macbook Air M1 2020 ', 'Sprzedam używanego Macbooka w bardzo dobrym stanie.\r\nCena 6000 zł', '2022-12-18 09:37:12', 1),
+(6, 'Fiat 126p', 'Rocznik:1979\r\nStan bardzo dobry\r\nCena: 31 000 zł', '2022-12-18 09:43:33', 1);
 
 -- --------------------------------------------------------
 
@@ -39,11 +55,17 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'Admin', 'admin@gmail.com', '$2y$10$reOd26W1DnqdWNwILizi/exuLeWP22V76HBLSHdhjYi4HKcwVIuTm');
+(1, 'Jasonj', 'admin@gmail.com', '$2y$10$reOd26W1DnqdWNwILizi/exuLeWP22V76HBLSHdhjYi4HKcwVIuTm');
 
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indeksy dla tabeli `ads`
+--
+ALTER TABLE `ads`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeksy dla tabeli `users`
@@ -56,12 +78,14 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT dla tabeli `ads`
+--
+ALTER TABLE `ads`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- INF04/Obiektowe_PHP/projekt_ogloszenia/ogloszenia1/ogloszenia/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-require('config.php');
-
-#var_dump($_GET);
-
-$requestUrl = "{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
-$bootstrap = new Bootstrap($requestUrl);
