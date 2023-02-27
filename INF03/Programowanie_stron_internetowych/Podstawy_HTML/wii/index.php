f25d1e329c4ceabc29cf19e47e3ded31be9e7f55
Jerzy Jasonek <jasonj2333@gmail.com>
2023-02-27 16:55:12 +0100

    Add Wii project

---
+++ INF03/Programowanie_stron_internetowych/Podstawy_HTML/wii/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Konsola Wii</title>
+    <style>
+        header, footer{
+            text-align: center;
+        }
+        main{
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <!-- Zaprojektuj stronę www dotyczącą konsoli Nintendo Wii, stronna powinna zawierać:
+    - nagłówek z bannerem - mozesz wykorzystać banner.png
+    - sekcje główną zawierającą:
+        - sekcje wprowadzającą z krótką historią konsoli
+        - sekcje ze specyfikacją techniczą z wykorzystaniem listy
+        - sekcje z przeglądem gier min. 10 (tabela zawierającą zdjęcie, nazwę grę, rok wydaniam, krótki opis, nazwy gier powinny być linkami do stron z internecie z pełnym opisem gry - strony te powinny otwierać się w niwych oknach)
+        - sekcje kontakt z formularzem przesyłanym metodą post do skryptu mail.php, zawierający pola:
+            - imię i nazwisko
+            - email
+            - wiadomość
+            - przyciski Prześlij, wyczyść
+    - stopkę zawierającą Copyright&copy; Imie nazwisko
+
+    Wykorzystaj znaczniki semantyczne.
+    Dodatkowo:
+    Do strony podepnij plik skrypt.js i spróbuj wykorzystać zawarte w nim funkcje pokaz, ukryj, powieksz, pomniejsz np. do obsługi zdarzenia zwijania i rozwijania historii konsoli i do zwiekszania i zmiejszania obrazkow.
+
+    Stronę tworzymy tylko z uzyciem HTML, budujemy strukturę a nie wygląd strony. 
+    Możesz dołożyć coś swojego, strona nie musi wyglądać dokładnie tak jak u mnie.
+
+    Możesz np. osadzić na stronie np. film z prezentacji konsoli:
+    - https://www.youtube.com/watch?v=73yDRm8KaWY
+    -->
+    
+</body>
+</html>
---
+++ INF03/Programowanie_stron_internetowych/Podstawy_HTML/wii/skrypt.js
@@ -0,0 +1,19 @@
+function pokaz(id) {
+    kontener = document.getElementById(id);
+    kontener.style.display = "block";
+}
+
+function ukryj(id) {
+    kontener = document.getElementById(id);
+    kontener.style.display = "none";
+}
+
+function powieksz(id){
+    kontener = document.getElementById(id);
+    kontener.style.width = "100%";
+}
+
+function zmniejsz(id){
+    kontener = document.getElementById(id);
+    kontener.style.width = "50%";
+}
