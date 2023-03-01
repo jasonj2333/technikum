04256511e66816221bc576f0a04a34eb0d3706fa
Jerzy Jasonek <jasonj2333@gmail.com>
2023-03-01 20:31:47 +0100

    Update Programowanie_aplikacji_webowych

--- INF03/Programowanie_aplikacji_internetowych/Egzamin/2021-EE.09_01_06_SG_kolor/przykladowe_rozwiazanie/sposob2/connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db = "ee_09_2021_01_06_sg_egzamin";
-    $conn = new mysqli($host,$user,$pass, $db);
-?>
---
+++ INF03/Programowanie_aplikacji_internetowych/Egzamin/sklep_eko/sklep.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Warzywniak</title>
+    <link rel="stylesheet" href="styl2.css">
+</head>
+<body>
+    <div class="banerL">
+        <h1>Internetowy sklep z eko-warzywami</h1>
+    </div>
+    <div class="banerP">
+        <ol>
+            <li>Warzwa</li>
+            <li>Owoce</li>
+            <li><a href="https://terapiasokami.pl/" target="_blank">Soki</a></li>
+        </ol>
+    </div>
+    <div class="glowny">
+        <!--sktypt1-->
+        <div class="gen">
+            <img src="gruszka.jpg" alt="warzywniak">
+            <p>opis: Klaps</p>
+            <p>na stanie: 20</p>
+            <h2>2.56zł</h2>
+        </div>
+        
+    </div>
+    <div class="stopka">
+        <form action="" method="post">
+            Nazwa: <input type="text" name="nazwa" id="nazwa">
+            Cena: <input type="number" name="cena" id="cena">
+            <input type="submit" value="Dodaj produkt"><br>
+            Stronę wykonał: 1234567890
+        </form>
+        <!--sktypt2-->
+        
+    </div>
+</body>
+</html>
