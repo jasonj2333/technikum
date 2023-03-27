d8975621f183477c6795e7cdeffce5a317a5b59b
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2023-03-27 08:43:46 +0200

    Add files via upload

--- INF04/Obiektowe_PHP/laravel/resources/views/pages/show.blade.php
+++ INF04/Obiektowe_PHP/laravel/resources/views/pages/show.blade.php
@@ -4,12 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spis stron</title>
+    <title>Strona {{ $title }}</title>
 </head>
 <body>
-    <h1>Spis treści</h1>
-    @foreach ($pages as $page)
-        <p>{{ $page->title }} - {{ $page->content }}</p>
-    @endforeach
+    <h1>Strona {{ $title }}</h1>
 </body>
 </html>
--- INF04/Obiektowe_PHP/laravel/resources/views/start.blade.php
+++ INF04/Obiektowe_PHP/laravel/resources/views/start.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="pl">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Strona startowa</title>
+    <title>Projekt Laravel</title>
 </head>
 <body>
-    <h1>Mój pierwszy projekt z Laravel</h1>
+    <h1>Strona główna</h1>
 </body>
 </html>
