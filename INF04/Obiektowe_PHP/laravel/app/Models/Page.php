b26b9179ab03dee5648a51afedee32a964b3be4d
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2023-03-27 08:42:36 +0200

    Add files via upload

--- INF04/Obiektowe_PHP/laravel/app/Models/Page.php
+++ INF04/Obiektowe_PHP/laravel/app/Models/Page.php
@@ -8,6 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Page extends Model
 {
     use HasFactory;
-    // protected $table = 'pages'; //Jeżeli tabela w bazie nazywa się inaczej niż model w liczbie mnogiej
-    //protected $fillable = ['title', 'slug', 'content'];
+    //protected $table = 'strony';
 }
