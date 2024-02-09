ebba528bc40576d05728248e4ae216b807f4f948
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2024-02-09 11:19:51 +0100

    Add files via upload

---
+++ INF03/Programowanie_aplikacji_internetowych/1TIP_prog/19_formularze/script.js
@@ -0,0 +1,7 @@
+//console.log(document.body);
+//console.log(document.forms[1][2]);
+//console.log(document.images);
+//console.log(document.links);
+//console.log(window);
+document.body.style.background = "beige";
+document.forms[0].style.background = "yellow"
