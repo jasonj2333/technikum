53f37510369715097e178b7bd2448d31851a5927
Jerzy Jasonek <jasonj2333@gmail.com>
2024-03-07 11:22:10 +0100

    add Users-UI to 1Tip in Inf03

---
+++ INF03/Programowanie_aplikacji_internetowych/1TIP_prog/22_userClass - UI/tools.js
@@ -0,0 +1,19 @@
+function stringToHash(string) {
+ 
+    let hash = 0;
+ 
+    if (string.length == 0) return hash;
+ 
+    for (let i = 0; i < string.length; i++) {
+        let char = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + char;
+        hash = hash & hash;
+    }
+ 
+    return hash;
+}
+
+
+function findUserByNick(list, nick, pass) {
+    return list.find((obj) => obj.nick === nick && obj.pass === pass);
+}
---
+++ INF03/Programowanie_aplikacji_internetowych/1TIP_prog/22_userClass - UI/user.js
@@ -0,0 +1,30 @@
+class User{
+    constructor(nick, name, surname, email, role){
+        this.nick = nick;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.role = role;
+        this.loginDates = [];
+        this.active = true;
+    }
+    login(){
+        let date = new Date();
+        this.loginDates.push(date.toLocaleString())
+    }
+    showLoginDates() {
+        this.loginDates.forEach((value, index) => {
+            console.log(index, value);
+        });
+    }
+    showUser(){
+        console.log(`Uzytkownik ${this.name} ${this.surname} -  ${this.nick} - ${this.email} - ${this.active}`);
+    }
+    toggleActive(){
+        this.active = !this.active;
+    }
+
+}
+
+const user1 = new User("jasonj", "Jerzy", "Jasonek", "jasonj@wp.pl", "admin");
+
