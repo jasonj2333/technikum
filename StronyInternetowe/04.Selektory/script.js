2c2b13d2279f66f6f03422b8cb2ab10818ae3f00
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-02-24 10:33:10 +0100

    Add files via upload

---
+++ StronyInternetowe/04.Selektory/script.js
@@ -0,0 +1,5 @@
+const header = document.querySelector("header");
+const sections = document.querySelectorAll("section");
+const klasy = document.querySelector("#klasy");
+const h3Klasy = klasy.querySelector("h3");
+console.log(h3Klasy);
