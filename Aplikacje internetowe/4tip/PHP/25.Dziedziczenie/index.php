8e0ebabe3d84935370f3691db56690c93cc24f5f
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-03-25 12:58:57 +0100

    Add files via upload

--- Aplikacje internetowe/4tip/PHP/25.Dziedziczenie/index.php
+++ Aplikacje internetowe/4tip/PHP/25.Dziedziczenie/index.php
@@ -1,8 +1,8 @@
 <?php 
 
 class Osoba{
-    public $imie;
-    public $wiek;
+    protected $imie;
+    protected $wiek;
 
     public function __construct($imie, $wiek){
         $this->imie = $imie;
@@ -15,8 +15,8 @@ class Osoba{
 }
 
 class Uczen extends Osoba{
-    public $klasa;
-    public $nr_leg;
+    private $klasa;
+    private $nr_leg;
 
     public function __construct($imie, $wiek, $klasa, $nr_leg){
         // $this->imie = $imie;
@@ -27,14 +27,51 @@ class Uczen extends Osoba{
     }
 
     public function Info(){
-        //echo "<p>Mam na imię ".$this->imie." i mam ".$this->wiek." lat</p>";
+        echo "<p>Mam na imię ".$this->imie." i mam ".$this->wiek." lat</p>";
+        //parent::Info();
+        echo "<p>Chodzę do klasa ".$this->klasa." numer legitymacji ".$this->nr_leg."</p>";
+    }
+}
+
+//Klasa nauczyciel
+//przedmiot
+
+class Nauczyciel extends Osoba{
+    protected $przedmiot;
+
+    public function __construct($imie, $wiek, $przedmiot){
+        parent::__construct($imie, $wiek);
+        $this->przedmiot = $przedmiot;
+    }
+
+    public function Info(){
         parent::Info();
-        echo "<p>Chodzę do klasa ".$this->klasa." numer legitymacji ".$this->nr_leg." lat</p>";
+        echo "<p>Uczę przedmiotu ".$this->przedmiot."</p>";
+    }
+}
+
+class Wychowawca extends Nauczyciel{
+    private $klasa;
+
+    public function __construct($imie, $wiek, $przedmiot, $klasa){
+        Osoba::__construct($imie, $wiek);
+        $this->przedmiot = $przedmiot;
+        $this->klasa = $klasa;
+    }
+
+    public function Info(){
+        parent::Info();
+        echo "<p>Jestem wychowawcą klasy ".$this->klasa."</p>";
     }
 }
 
 $osoba1 = new Osoba("Tomek", 18);
+//$osoba->imie = "Anna";
 $osoba1->Info();
 $uczen1 = new Uczen("Ala", 15, "1b", 1234);
+//$uczen1->klasa = "2c";
 $uczen1->Info();
-
+$nauczyciel1 = new Nauczyciel("Wojciech", 42, "chemia");
+$nauczyciel1->Info();
+$wychowawca1 = new Wychowawca("Anna", 35, "biologa", "4TIP");
+$wychowawca1->Info();
