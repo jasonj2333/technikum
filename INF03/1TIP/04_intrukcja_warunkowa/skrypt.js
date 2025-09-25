be36033d674ee279c601d79f7efcc0b360f9698b
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-09-25 09:38:18 +0200

    Add files via upload

---
+++ INF03/1TIP/04_intrukcja_warunkowa/skrypt.js
@@ -0,0 +1,10 @@
+let a = 15;
+let b = 13;
+
+if(a > b){
+    document.writeln(`Zmienna a=${a} jest większa od b=${b}`);
+}else{
+    document.writeln(`Zmienna a=${a} nie jest większa od b=${b}`);
+}
+
+document.writeln("Dalsza część programu");
