0da2bdfbc8d5393a26eab0cf147105e69823cac1
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-09-25 12:28:23 +0200

    Add files via upload

---
+++ INF03/3TI_2/03_instrukcja_warunkowa/skrypt1.js
@@ -0,0 +1,9 @@
+let wiek = -8;
+
+//Jeżeli wiek >= 18 - Możesz głosować w wyborach
+//w przeciwnym razie - Nie możesz głosować.
+if(wiek >= 18){
+    document.writeln("Możesz głosować.");
+}else{
+    document.writeln("Nie możesz głosować.");
+}
