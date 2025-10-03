e18e7d4931624f030aa538de646cfd3a3c3d1b4a
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-10-03 14:20:55 +0200

    Add files via upload

---
+++ INF04/2TIP/03/petle.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int poczatek = 5;
+    int koniec = 67;
+    int suma = 0;
+    int liczba = poczatek;
+
+    while (suma < koniec) {
+        suma += liczba;
+        liczba++;
+    }
+
+    cout << "Suma liczb wynosi: " << suma << endl;
+
+    int liczbaDni;
+
+    do {
+        cout << "Podaj liczbe dni w miesiacu: ";
+        cin >> liczbaDni;
+    } while (liczbaDni < 28 || liczbaDni > 31);
+
+    cout << "Liczba dni wynosi: " << liczbaDni << endl;
+
+    for (int i = 1; i <= liczbaDni; i++) {
+        cout << i << "\t";
+        if (i % 7 == 0) cout << endl;
+    }
+
+}
---
+++ INF04/2TIP/03/switch.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int dzien = 6;
+
+    switch (dzien) {
+    case 1:
+        cout << "Znowu poczatek tygodnia :(" << endl;
+        break;
+    case 2:
+        cout << "Wtorek, jeszcze tylko 3 dni i weekend" << endl;
+        break;
+    case 3:
+        cout << "Już środek tygodnia" << endl;
+        break;
+    case 4:
+        cout << "Czwartek" << endl;
+        break;
+    case 5:
+        cout << "Piatek" << endl;
+        break;
+    case 6:
+    case 7:
+        cout << "Weekend" << endl;
+        break;
+    default:
+        cout << "Nie znam takiego dnia tygodnia" << endl;
+        break;
+    }
+
+    //operator warunkowy
+    string komentarz = (dzien >= 1 && dzien <= 5) ? "Pracujemy" : "Wolne";
+    cout << komentarz << endl;
+}
+
