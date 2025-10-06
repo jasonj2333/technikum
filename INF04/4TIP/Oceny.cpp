6dc32e861e02c4ed5fcfafad9e8fea389661f42f
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-10-06 15:15:33 +0200

    Add files via upload

---
+++ INF04/4TIP/Oceny.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+template<class T, int n>
+class Oceny {
+public:
+    T oceny[n];
+    T max() {
+        T temp = oceny[0];
+        for (int i = 1; i < n; i++) {
+            if (oceny[i] > temp) temp = oceny[i];
+        }
+        return temp;
+    }
+    T min() {
+        T temp = oceny[0];
+        for (int i = 1; i < n; i++) {
+            if (oceny[i] < temp) temp = oceny[i];
+        }
+        return temp;
+    }
+};
+
+int main()
+{
+    Oceny<int, 4> oceny{ 3,4,6,3 };
+    cout << "Najwyzsza ocena: " << oceny.max() << endl;
+    cout << "Najnizsza ocena: " << oceny.min() << endl;
+}
+
