a112f9971d870acab458e38be36fbb0d0ee20682
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-11-04 10:28:46 +0100

    Add files via upload

---
+++ INF04/2TIP/05/tabliceStatyczne.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int n = 5;
+
+    float tablica[n];
+
+    float suma = 0;
+
+    cout << "Podaj wartosc elementow skladowych tablicy" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "tablica[" << i << "] = ";
+        cin >> tablica[i];
+    }
+
+    cout << "Elementy tablicy: " << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "tablica[" << i << "] = " << tablica[i] << endl;
+        suma += tablica[i];
+    }
+    //Tutaj wyswietl sume elementow
+    cout << "Suma elementow tablicy wynosi: " << suma << endl;
+    cout << "Srednia elementow tablicy wynosi: " << suma/n << endl;
+
+    tablica[3] = 0;
+
+    cout << "Elementy tablicy: " << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "tablica[" << i << "] = " << tablica[i] << endl;
+
+    }
+}
+
---
+++ INF04/2TIP/05/tabliceWielowymiarowe.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+const int w = 3; //wiersze
+const int k = 2; //kolumny
+
+int tablica[w][k];
+
+int main()
+{
+	for (int i = 0; i < w; i++)
+	{
+		for (int j = 0; j < k; j++) {
+			tablica[i][j] = i + j;
+		}
+	}
+
+	for (int i = 0; i < w; i++)
+	{
+		for (int j = 0; j < k; j++) {
+			cout << "Element[" << i << "][" << j << "] = " << tablica[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+
+	int tablica2[w][k] = { {1,2},{5,9},{3,11} }; //od C++11
+
+	for (int i = 0; i < w; i++)
+	{
+		for (int j = 0; j < k; j++) {
+			cout << "Element[" << i << "][" << j << "] = " << tablica2[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << endl;
+
+	int tablica3[w][k]{1,2,3,4,5,6}; //od C++11
+
+	for (int i = 0; i < w; i++)
+	{
+		for (int j = 0; j < k; j++) {
+			cout << "Element[" << i << "][" << j << "] = " << tablica3[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
