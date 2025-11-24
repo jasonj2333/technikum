02a7806157e15805101c224aad341d0c3b8ec927
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-11-24 16:02:08 +0100

    Add files via upload

--- INF03/2tip/07_funkcje/index.php
+++ INF03/2tip/07_funkcje/index.php
@@ -34,18 +34,38 @@ function iloczyn(int $a, int $b) : int {
 echo iloczyn(5, 7) . "<br>";
 echo iloczyn(12, 5) . "<br>";
 
-function iloraz(int $a, int $b) : float {
-    if($b == 0) return 0;
+function iloraz(int $a, int $b) : ?float {
+    if($b == 0) return null;
     return $a / $b;
 }
 
 echo iloraz(5,9) . "<br>";
-echo iloraz(5,0) . "<br>";
+if(iloraz(5,0) == null){
+    echo "Błąd dzielenia przez 0<br>";
+}else{
+    echo iloraz(5,0) . "<br>";
+}
+    
 
-function modulo(int $a, int $b) : int {
-    if($b == 0) return 0;
+function modulo(int $a, int $b) : ?int {
+    if($b == 0) return null;
     return $a % $b;
 }
 
 echo modulo(5,9) . "<br>";
 echo modulo(5,0) . "<br>";
+
+//Obsługa wyjątków
+function ilorazWyjatki(int $a, int $b) : float {
+    if($b == 0) throw new Exception("Błąd dzielenia przez 0!");
+    return $a / $b;
+}
+
+try{
+    echo ilorazWyjatki(5,7) . "<br>";
+    echo ilorazWyjatki(5,0) . "<br>"; //wyjątek powoduje przeskok do bloku catch
+}catch(Exception $e){
+    echo $e->getMessage();
+    echo "<br>\n W pliku: ".$e->getFile();
+    echo "<br>\n W linii: ".$e->getLine();
+}
