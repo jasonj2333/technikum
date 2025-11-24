1d71d7b3e1894ca5c14ae075f808e30d3f960842
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-11-24 15:12:07 +0100

    Add files via upload

---
+++ INF04/4TIP/wyjatki/komunikatyZwrotne.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+bool wejscieFormat(string sOcena) {
+    for (int i = 0; i < sOcena.length(); i++) {
+        if (isdigit(sOcena[i]) == false) return false;
+    }
+    return true;
+}
+
+bool wejscieZakres(int ocena) {
+    if (ocena >= 1 && ocena <= 6) return true;
+    return false;
+}
+
+int main()
+{
+    string sOcena = "6";
+
+    if (wejscieFormat(sOcena)) {
+        int ocena = stoi(sOcena);
+        if (wejscieZakres(ocena)) {
+            switch (ocena)
+            {
+                case 1:
+                    cout << "Uczeń nie otrzymuje promocji do następnej klasy..." << endl;
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                    cout << "Uczeń otrzymuje promocji do następnej klasy..." << endl;
+                    break;
+            }
+            
+        }
+        else {
+            cout << "Uwaga blad!!" << endl;
+            cout << "Wartosc nie miesci sie w zakresie <1, 6>" << endl;
+        }
+    }
+    else {
+        cout << "Uwaga blad!!" << endl;
+        cout << "Format nie odpowiada liczbie calkowitej" << endl;
+    }
+}
---
+++ INF04/4TIP/wyjatki/mechanizmWyjatkow.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        //Dane wejsciowe
+        double licznik = 1, mianownik = 0;
+        if (mianownik == 0) throw mianownik;
+
+        cout << "Licznik: " << licznik << endl;
+        cout << "Mianownik: " << mianownik << endl;
+        cout << "Iloraz: " << licznik / mianownik << endl;
+    }
+    catch (double blad) {
+        cout << "Blad dzielenia przez " << blad << endl;
+    }
+}
+
