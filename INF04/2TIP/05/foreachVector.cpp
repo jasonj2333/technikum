a5318abfa37be9e942d791a95bc1b26f5c2b8d69
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-11-25 10:32:28 +0100

    Add files via upload

---
+++ INF04/2TIP/05/foreachVector.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<double> wektor(5, 1);
+
+int main()
+{
+    cout << "Podaj wartosc elementow skladowych wektora: " << endl;
+    for (double& element : wektor) {
+        cout << "element = ";
+        cin >> element;
+    }
+
+    double suma{ 0 };
+    for (double element : wektor) {
+        cout << element << " " << endl;
+        suma += element;
+    }
+
+    cout << "Suma elementów wektora wynosi: " << suma << endl;
+    cout << "Srednia elementów wektora wynosi: " << suma / wektor.size() << endl;
+
+}
