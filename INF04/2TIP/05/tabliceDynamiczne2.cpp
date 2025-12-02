408f7361344c5dd1faf6b6ce49b331c1ce4b5bb7
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-12-02 10:18:27 +0100

    Add files via upload

---
+++ INF04/2TIP/05/tabliceDynamiczne2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Podaj ilosc elementow tablicy: ";
+    cin >> n;
+
+    int *w_tablica;
+    w_tablica = new int[n]; //pamięć ram - sterta
+
+    cout << "Podaj kolejne elementy tablicy: " << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "tablica[" << i << "] = ";
+        cin >> w_tablica[i]; // *(w_tablica + i)
+    }
+
+    int suma = 0;
+    for (int i = 0; i < n; i++) {
+        suma += w_tablica[i]; // *(w_tablica + i)
+    }
+
+    cout << "Suma elementow wynosi: " << suma << endl;
+
+    delete[] w_tablica; //zwolnienie pamięci
+}
+
