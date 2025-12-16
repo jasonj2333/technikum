e2120bf76483195e44b4e808a6910adcb9d6f707
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-12-16 10:35:21 +0100

    Add files via upload

---
+++ INF04/2TIP/06/klasaString.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string tekst;
+    string imie = "Jan";
+    string nazwisko("Kowalski");
+    string klasa{ "2TIP" };
+
+    //cout << "Podaj imie: ";
+    //cin >> imie;
+    cout << "Imie: " << imie << " Nazwisko: " << nazwisko << " klasa: " << klasa << endl;
+
+    cout << "Podaj jakis tekst: ";
+    cin.ignore();
+    getline(cin, tekst);
+    cout << tekst;
+
+    //Metody klasy string
+    string a = "Hello ";
+    string b = "World";
+    cout << a + b << endl;
+
+    cout << tekst.length() << endl;
+    cout << tekst.size() << endl;
+    cout << a[0] << a[3] << endl;
+
+    for (char znak : b) {
+        cout << znak << "-";
+    }
+    cout << endl;
+
+    string c = "abc";
+    string d = "abc";
+    cout << (c == d) << endl;
+    cout << (c != d) << endl;
+    cout << (c > d) << endl;
+    cout << c.compare(d) << endl;
+    cout << c.at(2) << endl;
+    cout << c.append("d") << endl;
+    cout << nazwisko.find("ski") << endl;
+}
+
