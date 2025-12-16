828c881eb6ac87444a890a309ec99597bbb58eb3
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2025-12-16 09:54:00 +0100

    Add files via upload

---
+++ INF04/06/OperacjeNaCNapisach.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char napis1[30] = "Community";
+    char napis2[] = "community";
+
+    cout << "Dlugosc 1 napisu: " << strlen(napis1) << endl;
+    cout << "Dlugosc 2 napisu: " << strlen(napis2) << endl;
+    cout << "Rozmiar napisu 1: " << sizeof(napis1) << endl;
+    cout << "Rozmiar napisu 2: " << sizeof(napis2) << endl;
+
+    cout << "Czy napisy sa takie same: " << (napis1 == napis2) << endl;
+}
+
