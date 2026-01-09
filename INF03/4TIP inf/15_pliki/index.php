743677a60edaf7fd5b46c5bd4c28bf0d4c93d061
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-01-09 09:38:46 +0100

    Add files via upload

---
+++ INF03/4TIP inf/15_pliki/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operacje na plikach</title>
+</head>
+<body>
+    <?php 
+        // touch('plik.txt');
+        // echo filesize('plik.txt');
+        // echo "<br>";
+        //unlink('plik.txt');
+
+        //$plik = fopen('plik.txt', 'w'); //do zapisu
+        $plik = fopen('plik.txt', 'a'); //dopisywanie
+        $text = "Jutro sobota, będzie wolne\n";
+        fwrite($plik, $text);
+        fclose($plik);
+
+        $plik = fopen('plik.txt', 'r'); //do odczytu - plik musi istnieć
+        
+        while(!feof($plik)){
+            $linia = fgets($plik, 100);
+            echo $linia ."<br>";
+        }
+        fclose($plik);
+    ?>
+</body>
+</html>
