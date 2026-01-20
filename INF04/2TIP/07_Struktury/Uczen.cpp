0eea49129b54d3069fe12b3954267f336c9b7ecb
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-01-20 10:53:53 +0100

    Add files via upload

--- INF04/2TIP/07_Struktury/Uczen.cpp
+++ INF04/2TIP/07_Struktury/Uczen.cpp
@@ -1,20 +1,41 @@
-﻿// Uczen.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+using namespace std;
+
+struct Uczen;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    struct Data {
+        //deklaracje pól (zmiennych członkowskich)
+        unsigned int dd, mm, rr;
+
+    };
+    
+    struct Uczen {
+        //deklaracje pól (zmiennych członkowskich)
+        string imie;
+        string nazwisko;
+        unsigned int nr_w_dzienniku;
+        Data data_urodzenia;
+        unsigned short oceny[3];
+    };
+
+    Uczen uczen1 = { "Jan", "Kowalski", 4, {30, 10, 2000}, {3, 4, 5} };
+
+    //prezentacja danych zapisanych w strukturze na ekranie monitora
+    cout << uczen1.imie << endl;
+    cout << uczen1.nazwisko << endl;
+    cout << uczen1.nr_w_dzienniku << endl;
+    //Wyświetlanie danych zapisanych w strukturze wewnętrzejne data_urodzenia należącej do typu Data
+    cout << uczen1.data_urodzenia.dd << endl;
+    cout << uczen1.data_urodzenia.mm << endl;
+    cout << uczen1.data_urodzenia.rr << endl;
+
+    //Wyświetlanie danych zapisanych tablicy oceny
+    cout << uczen1.oceny[0] << endl;
+    cout << uczen1.oceny[1] << endl;
+    cout << uczen1.oceny[2] << endl;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
