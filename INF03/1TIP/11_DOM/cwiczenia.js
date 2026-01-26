c4d94f8c792f4d299d0371846626a547f2fc5eb9
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-01-26 10:31:07 +0100

    Add files via upload

--- INF03/1TIP/11_DOM/cwiczenia.js
+++ INF03/1TIP/11_DOM/cwiczenia.js
@@ -15,3 +15,47 @@ h1.style.backgroundColor = "yellow";
 const firstArticle = document.querySelector(".articles article");
 const li = firstArticle.querySelectorAll("li");
 console.log(li);
+//li[0].style.color = "red";
+for(let i = 0; i < li.length; i++){
+    li[i].style.color = "red";
+}
+
+li.forEach(function(element){ //funkcja anonimowa
+    element.style.backgroundColor = "yellow";
+})
+
+li.forEach(element => { //funkcja strzałkowa
+    element.style.backgroundColor = "green";
+})
+
+//Pobieramy wszystkie paragrafy i ustalamy im kolor tekstu na niebieski
+const akapity = document.querySelectorAll("p");
+console.log(akapity);
+akapity.forEach(p => {
+    p.style.color = "blue";
+})
+
+//przycisk ze stopki
+const buttonFooter = document.querySelector("footer button");
+buttonFooter.innerText = "Przycisk";
+
+//Pobierz rodzica elementu z klasą "menu-item";
+const menuItem = document.querySelector(".menu-item");
+console.log(menuItem);
+const parentOfMenuItem = menuItem.parentElement;
+console.dir(parentOfMenuItem);
+
+//Dzieci elementu
+console.log(firstArticle.children[1]);
+console.log(firstArticle.firstElementChild);
+console.log(firstArticle.firstChild);
+console.log(firstArticle.lastElementChild);
+console.log(firstArticle.lastElementChild.previousElementSibling); //Poprzedzający element - rodzeństwo
+console.log(firstArticle.firstElementChild.nextElementSibling); //Następny element - rodzeństwo
+firstArticle.firstElementChild.nextElementSibling.style.color = "white";
+console.log(firstArticle.nextElementSibling);
+
+const secondArticle = document.querySelector("[data-id = '2']");
+console.log(secondArticle);
+secondArticle.style.backgroundColor = "beige";
+console.log(secondArticle.dataset.folder);
