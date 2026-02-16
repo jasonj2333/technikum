aea24daeafe769ff8c6b80891b20ccc67404972a
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-02-16 13:20:04 +0100

    Add files via upload

---
+++ INF04/4TIP/Algorytmy/BubbleSort2026.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+void bubbleSort(int tab[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool is_sorted = true;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (tab[j] > tab[j + 1]) {
+                int temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+                is_sorted = false;
+            }
+        }
+
+        if (is_sorted) break;
+    }
+}
+
+void wypisz(int tab[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << tab[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int tab[] = { 5, 3, 8, 4, 2 };
+    int n = 5;
+
+    wypisz(tab, n);
+    bubbleSort(tab, n);
+    wypisz(tab, n);
+}
+
