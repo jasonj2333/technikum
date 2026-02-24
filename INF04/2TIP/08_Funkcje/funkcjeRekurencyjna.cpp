e169b3710d67636e7cd76e0895f9b2649e1f9e71
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-02-24 10:36:04 +0100

    Add files via upload

---
+++ INF04/2TIP/08_Funkcje/funkcjeRekurencyjna.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int fib(int n) {
+    //przypadki podstawowe
+    if (n == 0) return 0;
+    if (n == 1 || n == 2) return 1;
+    //przypadek rekurencyjny
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main()
+{
+    cout << fib(5) << endl;
+    //fib(4)                 +                   fib(3)
+    //fib(3)        +   fib(2)                   fib(2) + fib(1)
+    //fib(2) + fib(1)   1                        1        1
+    //1        1
+    cout << fib(25) << endl;
+}
+
+
