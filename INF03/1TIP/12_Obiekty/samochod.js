8e65f859e0936f77b1a0453f1e53ba216a22f8f1
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-03-02 10:33:10 +0100

    Add files via upload

--- INF03/1TIP/12_Obiekty/samochod.js
+++ INF03/1TIP/12_Obiekty/samochod.js
@@ -10,16 +10,20 @@
 // hamuj(wartosc) – zmniejsza prędkość (nie może spaść poniżej 0)
 
 class Samochod{
+    //pola prywatne - nie widoczne poza klasą
+    #paliwo = 50;
+    #punkty = 1;
+    #przebieg = 1;
+
     constructor(marka, model, rok){
         this.marka = marka;
         this.model = model;
         this.rok = rok;
-        this.predkosc = 0;
+        this.predkosc = 1;
     }
 
     info(){
-        console.log(`${this.marka} ${this.model} ${this.rok}`);
-        console.log(`Aktualna prędkość: ${this.predkosc}`);
+        return `${this.marka} ${this.model} ${this.rok}`;
     }
 
     przyspiesz(wartosc){
@@ -32,11 +36,41 @@ class Samochod{
             this.predkosc = 0;
         }
     }
+
+    get paliwo(){
+        return this.#paliwo;
+    }
+
+    get punkty(){
+        return this.#punkty;
+    }
+
+    get przebieg(){
+        return this.#przebieg;
+    }
 }
 
 const skoda = new Samochod("Skoda", "Fabia", 2000);
-skoda.info();
-skoda.przyspiesz(100);
-skoda.info();
-skoda.hamuj(120);
-skoda.info();
+// skoda.info();
+// skoda.przyspiesz(100);
+// skoda.info();
+// skoda.hamuj(120);
+// skoda.info();
+const nazwa = document.querySelector("#nazwa");
+const predkosc = document.querySelector("#predkosc");
+const paliwo = document.querySelector("#paliwo");
+const przebieg = document.querySelector("#przebieg");
+const punkty = document.querySelector("#punkty");
+//Przyciski
+const gaz = document.querySelector("#gaz");
+const hamulec = document.querySelector("#hamulec");
+const jedz = document.querySelector("#jedz");
+const tankuj = document.querySelector("#tankuj");
+const hamuj = document.querySelector("#hamuj");
+
+//Aktualizacja
+nazwa.textContent = skoda.info();
+paliwo.textContent = skoda.paliwo;
+przebieg.textContent = skoda.przebieg;
+punkty.textContent = skoda.punkty;
+predkosc.textContent = skoda.predkosc;
