9ab97a194eea03147d89b8aa880bdb2c60082e26
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-03-09 08:06:20 +0100

    Add files via upload

---
+++ INF03/2tip/11_Komunikacja_DB/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "stolowka";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+$query = "SELECT imie, nazwisko, klasa FROM uczniowie;";
+$result = mysqli_query($conn, $query);
+
+echo "<h2>Fetch row</h2>";
+while($row = mysqli_fetch_row($result)){
+    echo "<p>$row[0] $row[1] - $row[2]</p>";
+}
+
+echo "<h2>Fetch assoc</h2>";
+$result = mysqli_query($conn, $query);
+while($row = mysqli_fetch_assoc($result)){
+    // echo "<p>".$row['imie']." ".$row['nazwisko']." - ".$row['klasa']."</p>";
+    echo "<p>{$row['imie']} {$row['nazwisko']} - {$row['klasa']}</p>";
+}
+mysqli_close($conn);
+
