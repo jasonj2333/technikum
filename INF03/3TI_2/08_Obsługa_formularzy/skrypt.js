57e4f149b02c1ff7ac960e9b4711d90a9c9c3ceb
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-04-09 15:11:00 +0200

    Add files via upload

--- INF03/3TI_2/08_Obsługa_formularzy/skrypt.js
+++ INF03/3TI_2/08_Obsługa_formularzy/skrypt.js
@@ -1,15 +1,43 @@
 const przycisk = document.querySelector("form button");
-console.log(przycisk);
+//console.log(przycisk);
+const wynik = document.querySelector("#wynik");
+const formularz = document.querySelector("form");
 
 przycisk.onclick = function(event){
     event.preventDefault();
+    wynik.innerHTML = "";
 
     const imie = document.querySelector("#imie");
     const nazwisko = document.querySelector("#nazwisko");
     const email = document.querySelector("#email");
     const haslo = document.querySelector("#haslo");
-    console.log(imie.value);
-    console.log(nazwisko.value);
-    console.log(email.value);
-    console.log(haslo.value);
+    let errors = 0;
+
+    if(imie.value.length < 3){
+        wynik.innerHTML += "<p>Imię musi mieć przynajmniej 3 znaki</p>";
+        errors++;
+    }
+    if(nazwisko.value.length < 3){
+        wynik.innerHTML += "<p>Nazwisko musi mieć przynajmniej 3 znaki</p>";
+        errors++;
+    }
+    if(email.value.length < 6){
+        wynik.innerHTML += "<p>Email musi mieć przynajmniej 6 znaków</p>";
+        errors++;
+    }
+    if(!email.value.includes("@") || !email.value.includes(".")){
+        wynik.innerHTML += "<p>Email musi zawierać znak @ i .</p>";
+        errors++;
+    }
+
+    if(haslo.value.length < 8){
+        wynik.innerHTML += "<p>Hasło musi mieć przynajmniej 8 znaków</p>";
+        errors++;
+    }
+
+    //console.log(wynik.innerHTML.length);
+    if(errors == 0){
+        wynik.innerHTML = "<p>Dane zostały wysłane</p>";
+        //formularz.submit();
+    }
 }
