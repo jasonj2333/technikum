043d02e3e3e8e9c60f7b0714f957b7559e762c92
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-04-09 09:40:29 +0200

    Add files via upload

--- INF03/1TIP/15_formularze/skrypt.js
+++ INF03/1TIP/15_formularze/skrypt.js
@@ -2,7 +2,11 @@ const buttonWyslij = document.querySelector("button[type='submit']");
 const formularz = document.querySelector("form");
 const bledy = document.querySelector("#bledy");
 const imie = document.querySelector("#imie");
-console.log(formularz);
+const nazwisko = document.querySelector("#nazwisko");
+const rodo = document.querySelector("#rodo");
+const email = document.querySelector("#email");
+//console.log(formularz);
+const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 // buttonWyslij.addEventListener("click", (e)=>{
 //     e.preventDefault();
@@ -11,8 +15,32 @@ console.log(formularz);
 
 formularz.addEventListener("submit", (e)=>{
     e.preventDefault();
+    bledy.innerHTML = "";
+    let errors = 0;
+
     if(imie.value.length < 3){
-        bledy.innerHTML = "<p>Imię musi mieć przynajmniej 3 znaki</p>";
+        bledy.innerHTML += "<p>Imię musi mieć przynajmniej 3 znaki</p>";
+        errors++;
     }
+
+    if(nazwisko.value.length < 3){
+        bledy.innerHTML += "<p>Nazwisko musi mieć przynajmniej 3 znaki</p>";
+        errors++;
+    }
+
+    // if(email.value.length < 6 || !email.value.includes("@") || !email.value.includes(".")){
+    //     bledy.innerHTML += "<p>Błędny email</p>";
+    // }
+
+    if(!regexEmail.test(email.value)){
+        bledy.innerHTML += "<p>Błędny email</p>";
+        errors++;
+    }
+
+    if(!rodo.checked){
+        bledy.innerHTML += "<p>Musisz wyrazić zgodę na przetwarzanie danych osobowych</p>";
+        errors++;
+    }
+
 });
 
