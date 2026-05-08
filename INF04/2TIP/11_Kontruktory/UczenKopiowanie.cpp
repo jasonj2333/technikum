45d0b39530a2e724a81b8696ae46d8a91436dead
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-05-08 13:51:37 +0200

    Add files via upload

---
+++ INF04/2TIP/11_Kontruktory/UczenKopiowanie.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Data {
+    int dd, mm, rr;
+};
+
+class Uczen {
+public:
+    string imie, nazwisko;
+    Data dataUrodzenia;
+    int* oceny;
+    int rozmiar;
+
+    Uczen() {};
+
+    Uczen(string imie, string nazwisko, Data dataUrodzenia) {
+        this->imie = imie;
+        this->nazwisko = nazwisko;
+        this->dataUrodzenia = dataUrodzenia;
+    }
+
+    /*Uczen(const Uczen& wzorzec) {
+        imie = wzorzec.imie;
+        nazwisko = wzorzec.nazwisko;
+        dataUrodzenia = { 1, 1, 2001 };
+    }*/
+
+    void wyswietlDane() {
+        cout << "Dane ucznia: " << endl;
+        cout << "Imie: " << imie << endl;
+        cout << "Nazwisko: " << nazwisko << endl;
+        cout << "Data urodzenia: " << dataUrodzenia.dd << "." << dataUrodzenia.mm << "." << dataUrodzenia.rr << endl;
+        for (int i = 0; i < rozmiar; i++) {
+            cout << oceny[i] << ", ";
+
+        }
+        cout << endl;
+    }
+
+    void dodajOceny(int ilosc) {
+        rozmiar = ilosc;
+        oceny = new int[rozmiar];
+        for (int i = 0; i < rozmiar; i++) {
+            cout << "Podaj ocene: ";
+            cin >> oceny[i];
+        }
+    }
+};
+
+int main()
+{
+    Uczen uczen1( "Anna", "Kowalska", {4, 5, 2002} );
+    uczen1.dodajOceny(4);
+    uczen1.wyswietlDane();
+    uczen1.oceny[0] = 1;
+    Uczen uczen2 = uczen1;
+    uczen2.dataUrodzenia.rr = 1992;
+    uczen2.wyswietlDane();
+    uczen2.oceny[0] = 5;
+    uczen1.wyswietlDane();
+    uczen2.wyswietlDane();
+}
---
+++ INF04/2TIP/11_Kontruktory/konstruktorKopiujacy.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Prostokat {
+    double bok1, bok2;
+public:
+    Prostokat();
+    Prostokat(double, double);
+    Prostokat(const Prostokat&); //konstruktor kopiujący
+
+    double pole();
+    double obwod();
+};
+
+int main()
+{
+    Prostokat prostokat1;
+    Prostokat prostokat2(5, 6);
+    Prostokat prostokat3(prostokat1); //konstruktor kopiujacy - wywolany jawnie
+    Prostokat prostokat4 = prostokat2;//konstruktor kopiujacy - wywolany niejawnie
+}
+
+Prostokat::Prostokat() {
+    cout << "Nastapilo wywolanie konstruktora domyslnego" << endl;
+    bok1 = 2;
+    bok2 = 2;
+}
+
+Prostokat::Prostokat(double bok1, double bok2) : bok1{ bok1 }, bok2{ bok2 } {
+    cout << "Nastapilo wywolanie konstruktora parametrycznego" << endl;
+}
+
+Prostokat::Prostokat(const Prostokat& wzorzec) {
+    cout << "Nastapilo wywolanie konstruktora kopiujacego" << endl;
+    bok1 = wzorzec.bok1;
+    bok2 = wzorzec.bok1;
+}
+
+double Prostokat::pole(){
+    return bok1 * bok2;
+}
+
+double Prostokat::obwod(){
+    return (bok1 + bok2) * 2;
+}
