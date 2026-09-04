e3943d4759aeeaa33e353d627d81f5efa54e1d4d
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-09-04 16:02:00 +0200

    Add files via upload

---
+++ inf03_26/4TI2/01_test3/proba.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strona w PHP</title>
+</head>
+<body>
+    <h1>Strona w PHP</h1>
+    <?php 
+        echo "<p>Tekst wygenerowany w PHP</p>";
+        echo "Kolejny tekst";
+    ?>
+</body>
+</html>
