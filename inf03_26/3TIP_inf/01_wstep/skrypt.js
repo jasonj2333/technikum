e87e733f7d353a4dfa7d8f30e6b73c9861565d31
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-09-07 13:15:02 +0200

    Add files via upload

---
+++ inf03_26/3TIP_inf/01_wstep/skrypt.js
@@ -0,0 +1,6 @@
+//alert("Działa");
+document.writeln(3+7);
+document.writeln("<p>Lubię informatykę</p>");
+console.log(189*67);
+console.log("Funkcja działa");
+document.body.style.backgroundColor = "red";
