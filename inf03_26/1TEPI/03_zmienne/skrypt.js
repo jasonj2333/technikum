c3a199be3b323c15653a9359450cd2165af6ab8b
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-09-10 16:34:27 +0200

    Add files via upload

---
+++ inf03_26/1TEPI/03_zmienne/skrypt.js
@@ -0,0 +1,22 @@
+//alert("Działa");
+
+/*
+Zmienna to wartość zapisana w pamięci komputera.
+- nazwę np. imie
+- adres z pamięci
+- wartość
+- typ
+- zakres
+- czas życia
+*/
+
+let imie = "Ania";
+//var nazwisko = "Kowalski"; //Raczej tak nie robimy
+let nazwisko = "Kowalska"; //tworzę zmienną i przypisuje wartość do zmiennej (definicja)
+console.log(imie, nazwisko);
+nazwisko = "Kowalska-Nowak"; //zmiena wartości
+console.log(imie, nazwisko);
+
+const rokUrodzenia = 2011;
+console.log(rokUrodzenia);
+//rokUrodzenia = 2013; //Błąd - nie można zmieniać wartości const
---
+++ inf03_26/1TEPI/03_zmienne/skrypt2.js
@@ -0,0 +1,19 @@
+let imieGoscia = prompt("Podaj imię: ");
+
+document.writeln("Witaj " + imieGoscia);
+
+let wiek = prompt("Ile masz lat? ");
+const urodziny = 2026 - wiek;
+
+document.writeln("To znaczy, że urodziłeś/łaś się w roku ", urodziny);
+
+let tekst = "Informatyka"; //string
+let a = 5.6; //number
+let czyZdal = true; //boolean (logiczny)
+let duzaLiczbaCalkowita = 23124423543645765876989324423 //BigInt
+
+//Undefined
+let zmienna;
+
+//Null
+let zmiennaNull = null;
