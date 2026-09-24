25c0da85c45794b3d3df40716d60ac4a8efb8e50
Jerzy Jasonek <48927964+jasonj2333@users.noreply.github.com>
2026-09-24 14:07:58 +0200

    Add files via upload

--- inf03_26/2TIP/04_operatory/index.php
+++ inf03_26/2TIP/04_operatory/index.php
@@ -21,6 +21,41 @@
 
         echo $b ** (1/3) ."<br>"; //Odpowiednik pierwiastkowania
         echo (int) ($a / $b); //Odpowiednik dzielenia całkowitego
+
+        //Operatory porównania i logiczne
+        echo "<br>";
+        var_dump($a == $b); //Porównanie wartości
+        var_dump(5 == '5');
+        echo "<br>";
+        var_dump(5 === '5'); //Porównanie typów i wartości
+        echo "<br>";
+        var_dump(!true); //negacja
+        echo "<br>";
+        var_dump($a != $b); //różne
+        echo "<br>";
+        var_dump($a > $b);
+        var_dump($a < $b);
+        var_dump($a >= $b);
+        var_dump($a <= $b);
+        echo "<br>";
+        var_dump($a > $b && $a > 25); //(i) koniukcja logiczna
+        var_dump($a > $b || $a > 25); //(lub) alternatywa logiczna
+
+        echo"<br>";
+        $a = $a + 5;
+        $a += 5;
+        $a -= 5;
+        $a *= 5;
+        $a /= 5;
+        $a %= 5;
+        $a++; //postinkrementacja
+        ++$a; //preinkrementacja
+        $a--; //postdekrementacja
+        --$a; //predekremetacja
+        echo "Moja liczba to " .++$a;
+
+        echo"<br>";
+        var_dump($a);
     ?>
 </body>
 </html>
